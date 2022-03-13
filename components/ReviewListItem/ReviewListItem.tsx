f173020ef0812a9aa13f91cf4638d3a5282e6d52
allenaxie <allenaxie1@gmail.com>
2022-03-13 04:09:37 -0700

    Create review cards and animate cards to flip on scroll.

--- components/ReviewListItem/ReviewListItem.tsx
+++ components/ReviewListItem/ReviewListItem.tsx
@@ -1,21 +1,52 @@
 import classes from './ReviewListItem.module.scss';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import {Button, CardActionArea, CardActions} from '@mui/material';
-
+import { Rating, Grid, Button, CardActionArea, CardActions, Avatar, Card, CardContent, CardMedia, Typography } from '@mui/material';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 interface ReviewListItemProps {
     review: any
 }
 
 const ReviewListItem = (props:ReviewListItemProps) => {
-    return (
-        <Card className={classes.card}>
-        {props.review.name}
-        {props.review.rating}
-        {props.review.comment}
+
+    useEffect(() => {
+        // all animations will take 2 seconds to complete
+        Aos.init({ duration: 2000 });
+    },[])
+
+    return (    
+        <Card className={classes.card} data-aos="flip-up">
+            <Grid container spacing={{xs:1}} columns={{xs:8}}>
+                <Grid item xs={3} className={classes.avatarContainer}>
+                    <Avatar 
+                    src={props.review.image}
+                    alt="profile image"
+                    className={classes.avatar}
+                    />
+                </Grid>
+                <Grid item xs={5} className={classes.contentContainer}>
+                    <CardContent className={classes.content}>
+                        <span className={classes.name}>
+                            {props.review.name}
+                        </span>
+                        <div className={classes.rating}>
+                            <Rating
+                            name={`${props.review.name}'s rating`}
+                            value={props.review.rating}
+                            readOnly
+                            size="large"
+                            />
+                            
+                        </div>
+                        <p className={classes.comment}>
+                            {props.review.comment}
+                        </p>
+
+                    </CardContent>
+
+                </Grid>
+            </Grid>
         </Card>
     )
 }
