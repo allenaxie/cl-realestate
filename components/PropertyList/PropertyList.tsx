ecfc16feba1612a9a3d10b132d3784dcd56db5f0
allenaxie <allenaxie1@gmail.com>
2022-03-12 21:22:29 -0800

    Create cards in properties section.

--- components/PropertyList/PropertyList.tsx
+++ components/PropertyList/PropertyList.tsx
@@ -1,14 +1,74 @@
 import classes from './PropertyList.module.scss';
 import PropertyListItem from '../PropertyListItem/PropertyListItem';
+import Grid from '@mui/material/Grid';
 
 const PropertyList = () => {
 
+    const properties = [
+        {
+            address: '1369 Londonderry Pl, Los Angeles, CA 90069',
+            images: 
+                {
+                    img1: 'https://photos.zillowstatic.com/fp/7e977d7153443dd13efa9e94888d81c0-uncropped_scaled_within_1536_1152.webp',
+                    img2: 'https://photos.zillowstatic.com/fp/72305199cafb73c60a74a274dcf0c3d9-cc_ft_768.webp',
+                    img3: 'https://photos.zillowstatic.com/fp/ee482a51ae6fb83de4952ca30c533dab-cc_ft_768.webp',
+                    img4: 'https://photos.zillowstatic.com/fp/429f8c5145e3dfe6ffb2a5e826cceadd-cc_ft_768.webp',
+                },
+            price: '$37,995,000',
+            description: '6 bed | 10 baths | 14,000 sq. ft.'
+        },
+        {
+            address: '58 Crested Cloud Way, Las Vegas, NV 89135',
+            images: 
+                {
+                    img1: 'https://photos.zillowstatic.com/fp/56da2a9165e220ae573f78be27fba078-cc_ft_1536.webp',
+                    img2: 'https://photos.zillowstatic.com/fp/72305199cafb73c60a74a274dcf0c3d9-cc_ft_768.webp',
+                    img3: 'https://photos.zillowstatic.com/fp/ee482a51ae6fb83de4952ca30c533dab-cc_ft_768.webp',
+                    img4: 'https://photos.zillowstatic.com/fp/429f8c5145e3dfe6ffb2a5e826cceadd-cc_ft_768.webp',
+                },
+            price: '$12,500,000',
+            description: '7 bed | 11 baths | 10,090 sq. ft.'
+        },
+        {
+            address: '54 Innisbrook Ave, Las Vegas, NV 89113',
+            images: 
+                {
+                    img1: 'https://photos.zillowstatic.com/fp/564db359a8076170d808a9bbb55d27df-cc_ft_1536.webp',
+                    img2: 'https://photos.zillowstatic.com/fp/72305199cafb73c60a74a274dcf0c3d9-cc_ft_768.webp',
+                    img3: 'https://photos.zillowstatic.com/fp/ee482a51ae6fb83de4952ca30c533dab-cc_ft_768.webp',
+                    img4: 'https://photos.zillowstatic.com/fp/429f8c5145e3dfe6ffb2a5e826cceadd-cc_ft_768.webp',
+                },
+            price: '$4,050,000',
+            description: '4 bed | 5 baths | 8,245 sq. ft.'
+        },
+        {
+            address: '4180 N Jensen St, Clark County, NV 89129',
+            images: 
+                {
+                    img1: 'https://photos.zillowstatic.com/fp/fc02e28b1abce6aa6d41cb308b5fad74-cc_ft_1536.webp',
+                    img2: 'https://photos.zillowstatic.com/fp/72305199cafb73c60a74a274dcf0c3d9-cc_ft_768.webp',
+                    img3: 'https://photos.zillowstatic.com/fp/ee482a51ae6fb83de4952ca30c533dab-cc_ft_768.webp',
+                    img4: 'https://photos.zillowstatic.com/fp/429f8c5145e3dfe6ffb2a5e826cceadd-cc_ft_768.webp',
+                },
+            price: '$5,995,000',
+            description: '8 bed | 9 baths | 8,900 sq. ft.'
+        },
+    ]
 
     
     return (
-        <>
-            <PropertyListItem/>
-        </>
+        <Grid 
+        container 
+        spacing={{ xs: 1, md:2}}
+        columns={{ xs:12, md:12 }}
+        className={classes.grid}
+        >
+            {properties.map(property => 
+            (<Grid item xs={12} md={6} className={classes.gridItem}>
+                <PropertyListItem property={property}/>
+            </Grid>)
+            )}
+        </Grid>
     )
 }
 
--- components/PropertyListItem/PropertyListItem.tsx
+++ components/PropertyListItem/PropertyListItem.tsx
@@ -1,10 +1,44 @@
 import classes from './PropertyListItem.module.scss';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import {Button, CardActionArea, CardActions} from '@mui/material';
 
-const PropertyListItem = () => {
+interface PropertyListItemProps {
+    property:any
+}
+
+interface CardProps {
+    component:string
+    height:string
+    image:string
+    alt:string
+}
+
+const PropertyListItem = (props: PropertyListItemProps) => {
+
     return (
-    <>
-        Property List Item
-    </>
+    <Card className={classes.card}>
+        <CardActionArea className={classes.actionArea}>
+            <Typography className={classes.price} gutterBottom variant="h5">{props.property.price}</Typography>
+            <CardMedia
+            component='img'
+            height='140'
+            image={props.property.images.img1}
+            alt='img 1'
+            className={classes.img}
+            />
+            <CardContent>
+                <Typography className={classes.address} component="div" variant="h5" gutterBottom>
+                    {props.property.address}
+                </Typography>
+                <Typography className={classes.description}>
+                    {props.property.description}
+                </Typography>
+            </CardContent>
+        </CardActionArea>
+    </Card>
     )
 }
 
