d012e0a9d3d149c0b3b6d73d7d5b7434a51cb7a3
allenaxie <allenaxie1@gmail.com>
2022-03-15 13:50:11 -0700

    Update review picture. Update footer icon size.

--- components/ReviewList/ReviewList.tsx
+++ components/ReviewList/ReviewList.tsx
@@ -11,10 +11,10 @@ const ReviewList = () =>  {
             comment: "Calvin is the best real estate agent I've ever seen. He is smart, patient, and works extremely hard to serve his clients. He helped me find my dream home within 30 days! I would recommend Calvin to everyone I know."
         },
         {
-            name: 'Adrian',
-            image: 'https://scontent.fsac1-2.fna.fbcdn.net/v/t1.6435-9/205385742_4752929548055622_6844612697171109551_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=862hh9XD59wAX9X7Yhu&_nc_ht=scontent.fsac1-2.fna&oh=00_AT9lyyTBGnbeVUJT2KkhgM5Y-N4Mj3iyF0dhHsuuGl3weg&oe=625306F9',
+            name: 'Jisoo',
+            image: 'images/jisoo-review.jpeg',
             rating: 5,
-            comment: "I love Calvin."
+            comment: "Calvin is an amazing agent and friend."
         }
     ]
 
